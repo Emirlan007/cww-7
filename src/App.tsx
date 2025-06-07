0aec20df3015f139c0327d6016b568bf046589df
Emirlan Kaldarbekov <kaldarbekovemirlan630@gmail.com>
2025-06-07 15:33:19 +0600

    Реализована логика подсчёта общей стоимости заказа а также добавлена заглушка при пустой корзине.

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,20 @@ import colaImage from "./assets/Cola.jpeg";
 const App = () => {
 
   const INGREDIENTS: Ingredient[] = [
-    {name: 'Hamburger', price: 80, image: burgerImage},
-    {name: 'Cheeseburger', price: 90, image: burgerImage},
-    {name: 'Fries', price: 50, image: friesImage},
-    {name: 'Coffee', price: 80, image: glassImage},
-    {name: 'Tea', price: 50, image: glassImage},
-    {name: 'Cola', price: 60, image: colaImage},
+    {name: 'Hamburger', price: 160, image: burgerImage},
+    {name: 'Cheeseburger', price: 180, image: burgerImage},
+    {name: 'Fries', price: 120, image: friesImage},
+    {name: 'Coffee', price: 100, image: glassImage},
+    {name: 'Tea', price: 60, image: glassImage},
+    {name: 'Cola', price: 80, image: colaImage},
   ];
 
   const [ingredients, setIngredients] = useState<SelectedIngredient[]>([
-    {name: 'Hamburger', price: 80, count: 0},
-    {name: 'Cheeseburger', price: 90, count: 0},
-    {name: 'Fries', price: 50, count: 0},
-    {name: 'Coffee', price: 80, count: 0},
-    {name: 'Tea', price: 80, count: 0},
+    {name: 'Hamburger', price: 160, count: 0},
+    {name: 'Cheeseburger', price: 180, count: 0},
+    {name: 'Fries', price: 120, count: 0},
+    {name: 'Coffee', price: 100, count: 0},
+    {name: 'Tea', price: 60, count: 0},
     {name: 'Cola', price: 80, count: 0},
   ]);
 
@@ -54,6 +54,22 @@ const App = () => {
     });
   };
 
+  let total = 0;
+
+  if (ingredients.length > 0 && INGREDIENTS.length > 0) {
+    total = INGREDIENTS.reduce((acc, ING) => {
+      ingredients.forEach(ing => {
+        if (ing.name === ING.name) {
+          acc = acc + ing.count * ING.price;
+        }
+      })
+      return acc;
+    },0);
+  }
+
+  const selectedIngredients = ingredients.filter(ing => ing.count > 0);
+
+
   return (
     <>
       <div className="main-container">
@@ -76,23 +92,24 @@ const App = () => {
 
         <div className="order-details">
           <h3>Order Details</h3>
-          {ingredients.map(ingred => (
-              <div
-                  key={ingred.name}
-                  className="items-list"
-              >
-                {ingred.count > 0 ?
-                    <p className="item-count">{ingred.name} <b style={{marginLeft: '10px'}}>x {ingred.count}</b>
-                    </p> : null}
-                {ingred.count > 0 ? <p>{ingred.price}</p> : null}
-                {ingred.count > 0 ?
-                    <button className="remove-btn" onClick={() => deleteIngredient(ingred.name)}>Remove</button> : null}
-              </div>
-          ))}
+
+          {selectedIngredients.length === 0 ? (
+              <p className="empty-cart">Order is empty <br/> Please add some items</p>
+          ) :
+              selectedIngredients.map(ingred => (
+                  <div key={ingred.name} className="items-list">
+                    <p className="item-name">{ingred.name}</p>
+                    <p className="item-count">x {ingred.count}</p>
+                    <p className="itemPrice">{ingred.price} KGS</p>
+                    <button className="remove-btn" onClick={() => deleteIngredient(ingred.name)}>Remove</button>
+                  </div>
+              ))
+          }
+          {total > 0 ? <p className="total-price"><b>Total price:</b> {total}</p> : null}
         </div>
       </div>
     </>
   )
-};
+}
 
 export default App;
