4c7d61d24bd5ab91318dbaa31f05b0d134eccec2
Emirlan Kaldarbekov <kaldarbekovemirlan630@gmail.com>
2025-06-10 14:55:51 +0600

    Вся стилизация находиться в App.css.Картинки и типизации добавлены в гит.Исправил путь импорта в main.tsx до App "./components/App.tsx.".В index.css убрал стили для кнопок.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -8,25 +8,25 @@ import colaImage from "../assets/Cola.jpeg";
 import IngredientButton from "../components/IngredientButton/IngredientButton.tsx";
 import OrderDetails from "../components/OrderDetails  /OrderDetails.tsx";
 
+const INGREDIENTS: Ingredient[] = [
+  {name: 'Hamburger', price: 160, image: burgerImage},
+  {name: 'Cheeseburger', price: 180, image: burgerImage},
+  {name: 'Fries', price: 120, image: friesImage},
+  {name: 'Coffee', price: 100, image: glassImage},
+  {name: 'Tea', price: 60, image: glassImage},
+  {name: 'Cola', price: 80, image: colaImage},
+];
+
+const [ingredients, setIngredients] = useState<SelectedIngredient[]>([
+  {name: 'Hamburger', price: 160, count: 0},
+  {name: 'Cheeseburger', price: 180, count: 0},
+  {name: 'Fries', price: 120, count: 0},
+  {name: 'Coffee', price: 100, count: 0},
+  {name: 'Tea', price: 60, count: 0},
+  {name: 'Cola', price: 80, count: 0},
+]);
+
 const App = () => {
-
-  const INGREDIENTS: Ingredient[] = [
-    {name: 'Hamburger', price: 160, image: burgerImage},
-    {name: 'Cheeseburger', price: 180, image: burgerImage},
-    {name: 'Fries', price: 120, image: friesImage},
-    {name: 'Coffee', price: 100, image: glassImage},
-    {name: 'Tea', price: 60, image: glassImage},
-    {name: 'Cola', price: 80, image: colaImage},
-  ];
-
-  const [ingredients, setIngredients] = useState<SelectedIngredient[]>([
-    {name: 'Hamburger', price: 160, count: 0},
-    {name: 'Cheeseburger', price: 180, count: 0},
-    {name: 'Fries', price: 120, count: 0},
-    {name: 'Coffee', price: 100, count: 0},
-    {name: 'Tea', price: 60, count: 0},
-    {name: 'Cola', price: 80, count: 0},
-  ]);
 
   const addIngredient = (nameIngred: string) => {
     setIngredients((prevState) => {
