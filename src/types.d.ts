e5213a526ced4873c4225db30a3abe5943bb01a2
Emirlan Kaldarbekov <kaldarbekovemirlan630@gmail.com>
2025-06-10 14:55:51 +0600

    Вся стилизация находиться в App.css.Картинки и типизации добавлены в гит.Исправил путь импорта в main.tsx до App "./components/App.tsx.".В index.css убрал стили для кнопок.

--- src/types.d.ts
+++ src/types.d.ts
@@ -9,3 +9,21 @@ export interface SelectedIngredient {
     count: number;
     price: number;
 }
+
+interface IngredientButtonProps {
+    ingredients: Ingredient[];
+    onAdd: (name: string) => void;
+}
+
+interface OrderItemProps {
+    name: string;
+    count: number;
+    price: number;
+    onRemove: () => void;
+}
+
+interface OrderDetailsProps {
+    ingredients: SelectedIngredient[];
+    deleteIngredient: (name: string) => void;
+    total: number;
+}
